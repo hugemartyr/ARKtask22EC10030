72ed5d2816c2772ef7c54533345b514c8165f9a2
hugemartyr <128484610+hugemartyr@users.noreply.github.com>
2023-07-03 20:05:59 +0530

    Add files via upload

---
+++ question2ark.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int main()
+{
+    char c[5];
+    int a[8],amt[8],change[8];
+
+    for(int i=0;i<8;i++)
+        {
+            a[i]=50;
+
+        }
+    printf("Enter the code for the drink of your choice:: ");
+    scanf("%s",c);
+
+    
+    
+        if(strcmp(c,"PEPS")==0)
+            {
+                printf("You have chosen Pepsi!\n");
+                printf("Enter 30 rs please:: ");
+                scanf("%d",&amt[0]);
+                if(amt[0]>=30)
+                    {
+                        printf("The change returned is:: %d\n Enjoy your Drink",amt[0]-30);
+                                       
+                    }
+
+                if(amt[0]<30)   
+                    {
+                        
+                       printf("The Amount entered is insufficient");
+
+                    }
+                    }
+            
+    
+
+    
+
+}
---
+++ test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    printf("Life's good");
+    return 0;
+}
